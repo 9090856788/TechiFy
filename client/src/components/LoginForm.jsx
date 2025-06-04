be4faa75337da97fd46d1a2edf4cc3c5803e33b3
Kanhu Charan Sahoo <kanhucharansahoo595@gmail.com>
2025-06-04 23:52:55 +0530

    integrate login APi

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/loginForm.css";
 import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import or from "../assets/or.png";
 import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const LoginForm = () => {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    emailOrUsername: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://user-authentication-system-backend-hazel.vercel.app/api/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const userRes = await response.json();
+      console.log("User Response:", userRes);
+      if (userRes.success) {
+        navigate("/");
+      } else {
+        alert(userRes.message);
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
 
   return (
     <>
@@ -42,8 +73,31 @@ const LoginForm = () => {
         </div>
         <form className="loginForm">
           <div className="loginForm_input">
-            <input type="text" placeholder="Enter Username/Email" />
-            <input type="password" placeholder="Enter Password" />
+            <input
+              type="text"
+              placeholder="Enter Username/Email"
+              value={formData.emailOrUsername}
+              onChange={(e) =>
+                setFormData({ ...formData, emailOrUsername: e.target.value })
+              }
+            />
+
+            <div className="password-wrapper">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter Password"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+              />
+              <span
+                className="toggle-password"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
           </div>
           <div className="loginForm_checkbox">
             <div>
@@ -53,10 +107,13 @@ const LoginForm = () => {
 
             <p onClick={() => navigate("/")}>Forgot Password?</p>
           </div>
-          <button type="submit" className="login_btn">Signin</button>
+          <button className="login_btn" onClick={handleLogin}>
+            Signin
+          </button>
         </form>
         <p className="loginForm_register">
-          Don't have an account? <span onClick={() => navigate("/register")}>Register</span>
+          Don't have an account?{" "}
+          <span onClick={() => navigate("/register")}>Register</span>
         </p>
       </div>
     </>
