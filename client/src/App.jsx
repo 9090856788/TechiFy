ec9dc0aee2df9a3ed2c0e02bf3f6f72a2f1cdefe
Kanhu Charan Sahoo <kanhucharansahoo595@gmail.com>
2025-06-09 18:27:00 +0530

    Define all default & admin routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import BlogLandingPage from "./pages/Blog/components/BlogLandingPage";
+import BlogPostView from "./pages/Blog/components/BlogPostView";
+import PostByTags from "./pages/Blog/components/PostByTags";
+import SearchPosts from "./pages/Blog/components/SearchPosts";
+import Dashboard from "./pages/Admin/components/Dashboard";
+import BlogPosts from "./pages/Admin/components/BlogPosts";
+import BlogPostEditor from "./pages/Admin/components/BlogPostEditor";
+import Comments from "./pages/Admin/components/Comments";
+import AdminLogin from "./pages/Admin/components/AdminLogin";
+import PrivateRoute from "./routes/PrivateRoute";
 
 const App = () => {
   return (
@@ -8,10 +18,34 @@ const App = () => {
       <Router>
         <Routes>
           {/* Default Routes */}
-          
+          <Route path="/" element={<BlogLandingPage />} />
+          <Route path="/:slug" element={<BlogPostView />} />
+          <Route path="/tag/:tagName" element={<PostByTags />} />
+          <Route path="/search" element={<SearchPosts />} />
+
           {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/posts" element={<BlogPosts />} />
+            <Route path="/admin/create" element={<BlogPostEditor />} />
+            <Route
+              path="/admin/edit/:postSlug"
+              element={<BlogPostEditor isEdit={true} />}
+            />
+            <Route path="/admin/comments" element={<Comments />} />
+            <Route path="/admin-login" element={<AdminLogin />} />
+          </Route>
         </Routes>
       </Router>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
     </>
   );
 };
--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
-const PrivateRoute = () => {
+const PrivateRoute = ({ allowedRoles }) => {
   return (
-    <div>PrivateRoute</div>
+    <>
+    <Outlet/>
+    </>
   )
 }
 
